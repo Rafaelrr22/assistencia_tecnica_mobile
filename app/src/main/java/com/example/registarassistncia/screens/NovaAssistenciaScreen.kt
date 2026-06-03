410e347f90cece3cfff9889b6b46218af17eea76
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-06-03 12:36:18 +0100

    Formulário inicial de nova assistência criado

--- app/src/main/java/com/example/registarassistncia/screens/NovaAssistenciaScreen.kt
+++ app/src/main/java/com/example/registarassistncia/screens/NovaAssistenciaScreen.kt
@@ -1,9 +1,79 @@
 package com.example.registarassistncia.screens
 
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
-fun NovaAssistenciaScreen(){
-    Text("Nova Assistencia")
+fun NovaAssistenciaScreen(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Nova Assistência",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        // Campo Cliente
+        var cliente by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = cliente,
+            onValueChange = { cliente = it },
+            label = { Text("Cliente") }
+        )
+
+        // Campo Equipamento
+        var equipamento by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value =  equipamento,
+            onValueChange = { equipamento = it },
+            label = {Text("Equipamento") }
+        )
+
+        // Campo Problema
+        var problema by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = problema,
+            onValueChange = { problema = it },
+            label = {Text("Problema") }
+        )
+
+
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {}
+        ) {
+            Text("Guardar")
+        }
+    }
 }
