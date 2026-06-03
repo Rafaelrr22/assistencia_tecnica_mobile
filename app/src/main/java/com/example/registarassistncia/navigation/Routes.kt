ae8e63b334e3497cd62d8e48858680085f398061
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-06-03 10:13:07 +0100

    Rotas da aplicação criadas

---
+++ app/src/main/java/com/example/registarassistncia/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.registarassistncia.navigation
+
+object Routes {
+    const val HOME = "home"
+    const val ASSISTENCIAS = "assistencias"
+    const val CLIENTES = "clientes"
+    const val EQUIPAMENTOS = "equipamentos"
+    const val NOVA_ASSISTENCIA = "nova_assistencia"
+}
