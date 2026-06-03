57fbbeb4dabe65c5f2c96cc5af4c7ceb02d73470
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-06-03 10:34:32 +0100

    Estrutura de navegação (AppNavigation) criada

--- app/src/main/java/com/example/registarassistncia/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/registarassistncia/navigation/AppNavigation.kt
@@ -1,3 +1,33 @@
 package com.example.registarassistncia.navigation
+
+
+import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.registarassistncia.HomeScreen
+
+@Composable
+fun AppNavigation() {
+
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME
+
+
+
+    ) {
+        composable(Routes.HOME) {
+            HomeScreen()
+        }
+        composable(Routes.ASSISTENCIAS) {}
+        composable(Routes.CLIENTES) {}
+        composable(Routes.EQUIPAMENTOS) {}
+        composable(Routes.NOVA_ASSISTENCIA) {}
+        composable(Routes.DETALHE_ASSISTENCIA) {}
+        composable(Routes.NOVO_CLIENTE) {}
+        composable(Routes.NOVO_EQUIPAMENTO) {}
+    }
+}
 
