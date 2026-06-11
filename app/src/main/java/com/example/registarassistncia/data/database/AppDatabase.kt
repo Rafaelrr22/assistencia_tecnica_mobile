3f35b6aa2cf3e076f3e2a7d9f0180ba030abd3e2
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-06-11 10:56:46 +0100

    AppDatabase criado

---
+++ app/src/main/java/com/example/registarassistncia/data/database/AppDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.registarassistncia.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.registarassistncia.data.dao.ClienteDao
+import com.example.registarassistncia.data.entity.AssistenciaEntity
+import com.example.registarassistncia.data.entity.ClienteEntity
+import com.example.registarassistncia.data.entity.EquipamentoEntity
+
+
+@Database(
+    entities = [
+        ClienteEntity::class,
+        EquipamentoEntity::class,
+        AssistenciaEntity::class,
+    ],
+
+    version = 1,
+    exportSchema = false
+)
+
+    abstract class AppDatabase : RoomDatabase() {
+
+        abstract fun clienteDao(): ClienteDao
+    }
