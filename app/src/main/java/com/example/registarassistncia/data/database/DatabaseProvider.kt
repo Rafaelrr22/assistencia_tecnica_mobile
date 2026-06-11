4c41a69c354238305381d306c0e57cff49cab31e
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-06-11 11:01:50 +0100

    Criado o DatabaseProvider

---
+++ app/src/main/java/com/example/registarassistncia/data/database/DatabaseProvider.kt
@@ -0,0 +1,27 @@
+package com.example.registarassistncia.data.database
+
+import android.content.Context
+import androidx.room.Room
+
+
+object DatabaseProvider {
+
+    @Volatile
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+
+        return INSTANCE ?: synchronized(this) {
+
+        val instance = Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "assistencia_db"
+        ).build()
+
+        INSTANCE = instance
+
+        instance
+        }
+    }
+}
